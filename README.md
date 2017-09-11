9147760b39b09ff331f8fd5e3e8a804e79e8f85d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-09-11 17:02:03 +0200

    Update 2017-09-11T15:01:59.759Z

---
+++ README.md
@@ -0,0 +1,31 @@
+Circle
+======
+
+Purpose: 
+- Using the web components technologies in projects.
+- Reducing boilerplate code
+- Doing approximatively what Angular does without its size.
+
+Installation
+------------
+```
+npm install @jlguenego/circle
+```
+
+Then, in your HTML file, import the circle HTML file.
+```
+<link rel="import" href="../node_modules/@jlguenego/circle/src/circle.html">
+```
+
+
+Table of Content
+================
+
+- [Getting started](./doc/GettingStarted.md)
+- [Concepts](./doc/Concepts.md)
+- [API Reference](./doc/APIReference.md)
+- [Examples](./doc/Examples.md)
+
+
+
+
---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+const gulp = require('gulp');
+const ghPages = require('gulp-gh-pages');
+const eslint = require('gulp-eslint');
+const debug = require('gulp-debug');
+const gulpIf = require('gulp-if');
+
+gulp.task('default', ['eslint']);
+
+const path = {
+	lint: ['**/*.js', '!node_modules/**/*', '!**/*.min.js'],
+	deploy: ['./*', '!./node_modules/**/*'],
+};
+
+
+gulp.task('eslint', function() {
+	return gulp.src(path.lint)
+		.pipe(debug())
+		.pipe(eslint())
+		.pipe(eslint.formatEach())
+		.pipe(eslint.failAfterError());
+});
+
+function isFixed(file) {
+	return file.eslint != null && file.eslint.fixed;
+}
+
+gulp.task('eslint-fix', function() {
+	return gulp.src(path.lint)
+		.pipe(eslint({
+			fix: true
+		}))
+		.pipe(eslint.formatEach())
+		.pipe(gulpIf(isFixed, gulp.dest('.')));
+});
+
+gulp.task('deploy', function() {
+	return gulp.src(path.deploy)
+		.pipe(ghPages({ cacheDir: '../.publish_boof'}));
+});
+
+
