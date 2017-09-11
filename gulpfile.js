8095eef02220a9391afbf3a1c2a8ba846add112b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-09-11 16:58:43 +0200

    add: gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
 const gulp = require('gulp');
+const ghPages = require('gulp-gh-pages');
 const eslint = require('gulp-eslint');
 const debug = require('gulp-debug');
 const gulpIf = require('gulp-if');
 
+
+const base = {
+	deploy: ['./src/**/*', './examples/**/*', 'index.html']
+}
 
 gulp.task('default', ['eslint']);
 
@@ -30,6 +35,11 @@ gulp.task('eslint-fix', function() {
 		}))
 		.pipe(eslint.formatEach())
 		.pipe(gulpIf(isFixed, gulp.dest('.')));
+});
+
+gulp.task('deploy', function() {
+	return gulp.src(base.deploy)
+		.pipe(ghPages({ cacheDir: '../.publish_boof'}));
 });
 
 
