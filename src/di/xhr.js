5a47fb97d9bf40d7f8eb2fe49a3f73410950fa98
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-09-13 18:21:58 +0200

    add: xhr

--- src/di/xhr.js
+++ src/di/xhr.js
@@ -1,7 +1,7 @@
 (function() {
 	'use strict';
 
-	class Http {
+	class XHR {
 		send(config) {
 			return new Promise((resolve, reject) => {
 				var xhr = new XMLHttpRequest();
@@ -61,7 +61,7 @@
 			});
 		}
 	}
-	const http = new Http();
-	circle.set('http', http);
+	const xhr = new XHR();
+	o.set('xhr', xhr);
 
 })();
