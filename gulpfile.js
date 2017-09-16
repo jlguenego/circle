6ca5148983f9a3702d0d4cb369859c669a5709be
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-09-16 13:07:50 +0200

    add: gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,49 @@ const ghPages = require('gulp-gh-pages');
 const eslint = require('gulp-eslint');
 const debug = require('gulp-debug');
 const gulpIf = require('gulp-if');
+const uglifyjs = require('uglify-es');
+const minify = require('gulp-uglify/composer')(uglifyjs, console);
+const pump = require('pump');
+const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
+const del = require('del');
 
+gulp.task('default', ['build']);
 
-gulp.task('default', ['eslint']);
-
-const path = {
+const pathCfg = {
 	lint: ['**/*.js', '!node_modules/**/*', '!**/*.min.js'],
-	circle: ['./src/circle.js'],
+	circle: './src/circle.js',
 	deploy: ['./**/*', '!./node_modules/**/*', '!package*', '!*.js'],
 	dist: 'dist',
 };
 
+gulp.task('clean', function () {
+	return del(pathCfg.dist);
+});
+
+gulp.task('copy', function () {
+	return gulp.src('./src/circle.js').pipe(gulp.dest('dist'));
+});
+
+gulp.task('uglify', ['copy'], function (cb) {
+	// the same options as described above
+	const options = {};
+
+	pump([
+		gulp.src('./src/circle.js'),
+		sourcemaps.init(),
+		minify(options),
+		rename('circle.min.js'),
+		sourcemaps.write('.'),
+		gulp.dest('dist')
+	],
+		cb
+	);
+});
+
+
 gulp.task('eslint', function () {
-	return gulp.src(path.lint)
+	return gulp.src(pathCfg.lint)
 		.pipe(debug())
 		.pipe(eslint())
 		.pipe(eslint.formatEach())
@@ -27,7 +57,7 @@ function isFixed(file) {
 }
 
 gulp.task('eslint-fix', function () {
-	return gulp.src(path.lint)
+	return gulp.src(pathCfg.lint)
 		.pipe(eslint({
 			fix: true
 		}))
@@ -36,8 +66,8 @@ gulp.task('eslint-fix', function () {
 });
 
 gulp.task('deploy', function () {
-	return gulp.src(path.deploy)
-		.pipe(ghPages({ cacheDir: '../.publish_boof' }));
+	return gulp.src(pathCfg.deploy)
+		.pipe(ghPages({ cacheDir: '../.publish_circle' }));
 });
 
 
