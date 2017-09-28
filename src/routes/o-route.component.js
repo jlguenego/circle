7f5f42268e4929dcc2ed0e52878ba4b5ea261da8
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-09-28 19:55:37 +0200

    add: route

---
+++ src/routes/o-route.component.js
@@ -0,0 +1,10 @@
+(function() {
+	'use strict';
+
+	class ORoute extends o.Element {
+		render() {
+			
+		}
+	}
+	ORoute.reg;
+})();
---
+++ src/routes/routes.di.js
@@ -0,0 +1,13 @@
+(function() {
+	'use strict';
+
+	class Routes {
+		config(config) {
+			this.config = config;
+		}
+
+		
+	}
+	o.di('routes', new Routes());
+
+})();
