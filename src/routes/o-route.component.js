9e49e3be8633fb9577c50c715d43963694df6b67
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-09-28 19:59:28 +0200

    add: o-route

--- src/routes/o-route.component.js
+++ src/routes/o-route.component.js
@@ -2,6 +2,11 @@
 	'use strict';
 
 	class ORoute extends o.Element {
+
+		init() {
+			const config = o.di('routes').config;
+			console.log('config', config);
+		}
 		render() {
 			this.root.innerHTML = 'coucou';
 		}
