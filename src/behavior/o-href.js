16e858119958583dcf3aab7780e35f342020402a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-09-28 19:43:51 +0200

    add: o-route

--- src/behavior/o-href.js
+++ src/behavior/o-href.js
@@ -1,8 +1,16 @@
 (function () {
     'use strict';
     console.log('ohref decl');
-    
+
     class OHref extends o.Behavior {
+
+        init() {
+            const behavior = this;
+            this.elt.addEventListener('click', function (event) {
+                window.history.pushState('object or string', 'title', behavior.host.getModel(behavior.key));
+                event.preventDefault();
+            });
+        }
 
         onDigest(key) {
             this.render();
