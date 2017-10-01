d437303a3bcf1473d6fe9df7d50bd84790752abd
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-10-01 12:46:00 +0200

    refactor

--- src/routes/routes.di.js
+++ src/routes/routes.di.js
@@ -2,8 +2,8 @@
 	'use strict';
 
 	class Routes {
-		config(config) {
-			this.config = config;
+		states(states) {
+			this.states = states;
 		}
 
 		register(elt) {
@@ -13,7 +13,7 @@
 		onClick(event, url) {
 			window.history.pushState('object or string', 'title', url);
 			event.preventDefault();
-			const component = this.config.find(n => n.url === url).component;
+			const component = this.states.find(n => n.url === url).component;
 			this.elt.root.innerHTML = '';
             this.elt.root.appendChild(document.createElement(component));
 		}
