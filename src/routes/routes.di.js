404356479ae961d9a62f9fc7408fd4d694724be1
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-10-01 13:18:02 +0200

    add: no need base

--- src/routes/routes.di.js
+++ src/routes/routes.di.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
 	'use strict';
 
 	class Routes {
@@ -9,27 +9,34 @@
 		register(elt) {
 			this.elt = elt;
 
-			const currentUrl = document.baseURI;
+			const currentUrl = window.location.href;
 			console.log('currentUrl', currentUrl);
 			let state = this.states.find(n => currentUrl.endsWith(n.url.substring(1)));
+			if (state) {
+				const baseElt = document.createElement('base');
+				baseElt.setAttribute('href', currentUrl);
+				document.head.insertBefore(baseElt, document.head.childNodes[0]);
+				this.elt.root.innerHTML = '';
+				this.elt.root.appendChild(document.createElement(state.component));
+			}
 			if (!state) {
-				state = this.states.find(n => n.default === true).component;
+				state = this.states.find(n => n.default === true);
+				this.goto(state.url);
 			}
 			if (!state) {
 				return;
 			}
+
 			
-			this.elt.root.innerHTML = '';
-            this.elt.root.appendChild(document.createElement(state.component));
 		}
 
 		goto(url) {
-			window.history.pushState('object or string', 'title', url);		
+			window.history.pushState('object or string', 'title', url);
 			const state = this.states.find(n => n.url === url);
 			this.elt.root.innerHTML = '';
-            this.elt.root.appendChild(document.createElement(state.component));
+			this.elt.root.appendChild(document.createElement(state.component));
 		}
-		
+
 	}
 	o.di('routes', new Routes());
 
