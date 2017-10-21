e2a62358d6fa207ae0e2b6782144f1b66b777461
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-10-21 19:17:03 +0200

    karma

--- test/example.js
+++ test/example.js
@@ -1,8 +1,7 @@
-var assert = require('assert');
 describe('Array', function() {
 	describe('#indexOf()', function() {
 		it('should return -1 when the value is not present', function() {
-			assert.equal(-1, [1, 2, 3].indexOf(4));
+			expect([1, 2, 3].indexOf(4)).to.be(-1);
 		});
 	});
 });
